94a5ba3147ea942f3b3152a7f131a7617028cbc0
EC2 Default User <ec2-user@ip-172-31-20-51.us-east-2.compute.internal>
2019-11-10 23:48:26 +0000

    gmail account change

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,15 +54,15 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'amitabanchhor@gmail.com'}
+  config.action_mailer.default_options = {from: 'cordcuttingcalculator@gmail.com'}
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
   domain:               'gmail.com',
-  user_name:            'amitabanchhor',
-  password:             'passforrails',
+  user_name:            'cordcuttingcalculator',
+  password:             'passwordforapp',
   authentication:       'plain',
   enable_starttls_auto: true  }
 end

4d63bcc60862ec4302afb4d7f2ecc620553e0cfd
EC2 Default User <ec2-user@ip-172-31-25-9.us-east-2.compute.internal>
2019-11-15 23:46:29 +0000

    ff

--- app/models/user.rb
+++ app/models/user.rb
@@ -82,16 +82,19 @@ class User < ApplicationRecord
   #   SecureRandom.hex(10)
   # end
   def send_password_reset
-    generate_token(:password_reset_token)
+    token=generate_token(:password_reset_token)
     #self.password_reset_sent_at = Time.zone.now
     self.update_columns(password_reset_sent_at: Time.zone.now)
+    self.update_columns(password_reset_token: token)
     UserMailer.forgot_password(self).deliver# This sends an e-mail with a link for the user to reset the password
   end
 # This generates a random password reset token for the user
   def generate_token(column)
-    begin
-      self[column] = SecureRandom.urlsafe_base64
-    end while User.exists?(column => self[column])
+    return SecureRandom.urlsafe_base64
+    # begin
+      
+    #   self[column] = SecureRandom.urlsafe_base64
+    # end while User.exists?(column => self[column])
   end
 
 

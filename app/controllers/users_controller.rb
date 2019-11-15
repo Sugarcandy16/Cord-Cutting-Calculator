96e7496de3709152809fc9350b7080cf2fe37f91
EC2 Default User <ec2-user@ip-172-31-25-9.us-east-2.compute.internal>
2019-11-15 19:33:24 +0000

    test8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,10 +49,10 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-     # UserMailer.welcome_email(@user).deliver_now
+      UserMailer.welcome_email(@user).deliver_now
       log_in @user
       @user.update_attributes(admin: true) if params[:user][:admin] == '1'
-      flash[:success] = "Sign up success!!!!! "+ @user.email
+      flash[:success] = "Sign up success! "
       redirect_to @user
     else
       render 'new'

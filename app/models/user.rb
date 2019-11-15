20004e8ee986ebc0278c8533ba9938a6691462ee
EC2 Default User <ec2-user@ip-172-31-25-9.us-east-2.compute.internal>
2019-11-15 21:02:55 +0000

    reset_pwd4

--- app/models/user.rb
+++ app/models/user.rb
@@ -85,7 +85,8 @@ class User < ApplicationRecord
     generate_token(:password_reset_token)
     self.password_reset_sent_at = Time.zone.now
     self.save
-    UserMailer.forgot_password(self).deliver# This sends an e-mail with a link for the user to reset the password
+    flash[:notice]='new'+@user.email
+    #UserMailer.forgot_password(self).deliver# This sends an e-mail with a link for the user to reset the password
   end
 # This generates a random password reset token for the user
   def generate_token(column)

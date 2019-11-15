85166e427494f013274a2ebab6125122631a66aa
EC2 Default User <ec2-user@ip-172-31-25-9.us-east-2.compute.internal>
2019-11-15 20:43:04 +0000

    reset_pwd3

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -3,7 +3,7 @@ class PasswordResetsController < ApplicationController
     user = User.find_by_email(params[:email])
     user.send_password_reset if user
     flash[:notice] = 'E-mail sent with password reset instructions.'
-    redirect_to new_session_path
+    redirect_to home_path
   end
   def edit
     @user = User.find_by_password_reset_token!(params[:id])
@@ -15,7 +15,7 @@ class PasswordResetsController < ApplicationController
       redirect_to new_password_reset_path
     elsif @user.update(user_params)
       flash[:notice] = 'Password has been reset!'
-      redirect_to new_session_path
+      redirect_to home_path
     else
       render :edit
     end

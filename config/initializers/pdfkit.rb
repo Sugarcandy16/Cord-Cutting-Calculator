e0b79bce1478898b4f46e365b96071f7ddff70e3
EC2 Default User <ec2-user@ip-172-31-28-103.us-east-2.compute.internal>
2019-12-05 23:23:14 +0000

    pdfgen2

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,3 +1,5 @@
-PDFKit.configure do |config|
-    config.wkhtmltopdf=rails.root.join('bin','wkhtmltopdf').to_s
+class Application < Rails::Application
+    PDFKit.configure do |config|
+        config.wkhtmltopdf=Rails.root.join('bin','wkhtmltopdf').to_s
+    end
 end

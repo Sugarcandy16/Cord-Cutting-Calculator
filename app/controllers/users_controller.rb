bdf27d519b24916adaf3c926c39908cac7cdadca
EC2 Default User <ec2-user@ip-172-31-28-103.us-east-2.compute.internal>
2019-12-05 23:42:01 +0000

    pdfgen5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -135,7 +135,13 @@ class UsersController < ApplicationController
 
     redirect_to result_path(params[:id], params[:flag_one_pack], params[:flag_dvr], params[:budget])
   end
-
+      
+  def pdfgen(html)
+      kit = PDFKit.new(html, :page_size => 'Letter')
+      pdf = kit.to_pdf
+      file = kit.to_file('/result.pdf')
+      File.open("result.pdf","w"){|f| f.write(data)}
+  end
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
@@ -169,11 +175,5 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
       redirect_to(root_url) unless @user == current_user || admin?
     end
-    
-    def pdfgen(html)
-      kit = PDFKit.new(html, :page_size => 'Letter')
-      pdf = kit.to_pdf
-      file = kit.to_file('/result.pdf')
-      File.open("result.pdf","w"){|f| f.write(data)}
-    end
+
 end

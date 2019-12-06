418e2b57e97d004ac6713b4a1baf043a2d035d37
EC2 Default User <ec2-user@ip-172-31-28-103.us-east-2.compute.internal>
2019-12-06 01:34:01 +0000

    pdfgen7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -143,6 +143,7 @@ class UsersController < ApplicationController
         file = kit.to_file('tmp/out.pdf')
         Download.new(file)
     end
+  end
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user

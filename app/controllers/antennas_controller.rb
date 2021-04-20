1474e694cdaf928998a19acda5d661a8e656e9db
EC2 Default User <ec2-user@ip-172-31-34-107.us-east-2.compute.internal>
2021-04-20 00:14:05 +0000

    reset implementation in own antenna pg3

--- app/controllers/antennas_controller.rb
+++ app/controllers/antennas_controller.rb
@@ -13,6 +13,8 @@ class AntennasController < ApplicationController
       @user.antennas.each do |antenna|
         @antennas << antenna.channel_id
       end
+    else
+      redirect_to show_antenna_path
     end
   end
   

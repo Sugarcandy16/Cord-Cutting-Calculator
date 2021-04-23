0b9eb2cedd2b132eb9d725fd1a63ed2f5fc16885
EC2 Default User <ec2-user@ip-172-31-32-84.us-east-2.compute.internal>
2021-04-23 15:41:02 +0000

    Search bar maybe x3?

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,6 @@ class UsersController < ApplicationController
   # GET /users.json
   def index
     @users = User.paginate(page: params[:page], per_page: 10)
-    @channels = Channel.search(params[:search])
   end
 
   # GET /users/1
--- app/models/user.rb
+++ app/models/user.rb
@@ -56,13 +56,5 @@ class User < ApplicationRecord
       end
     end
   end
-  
-  def self.search(search)
-  if search
-    find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
-  else
-    find(:all)
-  end
-end
 
 end

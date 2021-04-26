25a14e0928221ff3b9f1f753fbd8a50e53117f7c
EC2 Default User <ec2-user@ip-172-31-33-228.us-east-2.compute.internal>
2021-04-26 08:18:36 +0000

    updating reset story3

--- app/controllers/antennas_controller.rb
+++ app/controllers/antennas_controller.rb
@@ -13,8 +13,6 @@ class AntennasController < ApplicationController
       @user.antennas.each do |antenna|
         @antennas << antenna.channel_id
       end
-    else
-      render action: :show
 
     end
   end
@@ -23,7 +21,7 @@ class AntennasController < ApplicationController
     user = User.find(params[:id])
     Antenna.delete_record(params[:id])
     if params[:items] == nil
-      render action: :show
+      
     else
       Antenna.create_record(params[:id], params[:items])
       redirect_to user
